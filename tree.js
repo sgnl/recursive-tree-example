3c1e8cb12503554b6db465d2a445ec00acd6655f
Ray Farias <ray@rayfarias.com>
2014-09-13 13:54:20 -1000

    fix tree.js errors

--- tree.js
+++ tree.js
@@ -1,5 +1,3 @@
-
-
 'use strict';
 // a tree to be climbed.
 module.exports = {
@@ -8,273 +6,224 @@ module.exports = {
     age: 28,
     has_parent: null,
     has_children: true,
-    children: [
-      {
-        name: "Manny",
-        age: 22,
-        has_parent: true,
-        is_leaf: false,
-        has_children: true,
-        children: [
-          {
-            {
-              name: "Spencer Toyama",
-              age: 28,
-              has_parent: false,
-              is_leaf: true,
-              has_children: false,
-              children: []
-            },
-            {
-              name: "Shannon",
-              age: 65,
-              has_parent: true,
-              is_leaf: false,
-              has_children: true,
-              children: [
-                {
-                  name: "Kingtak",
-                  age: 302,
-                  has_parent: true,
-                  is_leaf: false,
-                  has_children: true,
-                  children: [
-                    {
-                      name: "Wray",
-                      age: 1337,
-                      has_parent: true,
-                      is_leaf: false,
-                      has_children: true,
-                      children: [
-                        {
-                          name: "Edgar Allan Poe",
-                          age: 560,
-                          has_parent: true,
-                          is_leaf: true,
-                          has_children: false,
-                          children: []
-                        },
-                        {
-                          name: "Albert Einstein",
-                          age: 91,
-                          has_parent: true,
-                          is_leaf: true,
-                          has_children: false,
-                          children: []
-                        },
-                      ]
-                    },
-                    {
-                      name: "Nikola Tesla",
-                      age: 16,
-                      has_parent: true,
-                      is_leaf: true,
-                      has_children: false,
-                      children: []
-                    }
-                  ]
-                },
-                {
-                  name: "Peter",
-                  age: 36,
-                  has_parent: true,
-                  is_leaf: false,
-                  has_children: true,
-                  children: [
-                    {
-                      name: "Garfield The Cat",
-                      age: 783,
-                      has_parent: true,
-                      is_leaf: true,
-                      has_children: false,
-                      children: []
-                    },
-                    {
-                      name: "Cortney",
-                      age: 243,
-                      has_parent: true,
-                      is_leaf: false,
-                      has_children: true,
-                      children: [
-                        {
-                          name: "Brian",
-                          age: 32,
-                          has_parent: true,
-                          is_leaf: false,
-                          has_children: true,
-                          children: [
-                            {
-                              name: "Eylem",
-                              age: 24,
-                              has_parent: true,
-                              is_leaf: false,
-                              has_children: true,
-                              children: [
-                                {
-                                  name: "Milton S. Hershey",
-                                  age: 8008,
-                                  has_parent: true,
-                                  is_leaf: true,
-                                  has_children: false,
-                                  children: []
-                                }
-                              ]
-                            },
-                            {
-                              name: "Albert",
-                              age: 87,
-                              has_parent: true,
-                              is_leaf: false,
-                              has_children: true,
-                              children: [
-                                {
-                                  name: "Charles",
-                                  age: 0,
-                                  has_parent: true,
-                                  is_leaf: false,
-                                  has_children: true,
-                                  children: [
-                                    {
-                                      name: "Gene Shoemaker",
-                                      age: 62,
-                                      has_parent: true,
-                                      is_leaf: true,
-                                      has_children: false,
-                                      children: []
-                                    }
-                                  ]
-                                },
-                                {
-                                  name: "Neil deGrasse Tyson",
-                                  age: 65,
-                                  has_parent: true,
-                                  is_leaf: true,
-                                  has_children: false,
-                                  children: []
-                                }
-                              ]
-                            }
-                          ]
-                        },
-                        {
-                          name: "Carl Sagan",
-                          age: 23,
-                          has_parent: true,
-                          is_leaf: true,
-                          has_children: false,
-                          children: []
-                        }
-                      ]
-                    },
-                    {
-                      name: "Mark",
-                      age: 100,
-                      has_parent: true,
-                      is_leaf: false,
-                      has_children: true,
-                      children: [
-                        {
-                          name: "Marty McFly",
-                          age: 43,
-                          has_parent: true,
-                          is_leaf: true,
-                          has_children: false,
-                          children: []
-                        },
-                        {
-                          name: "Jayson",
-                          age: 817,
-                          has_parent: true,
-                          is_leaf: false,
-                          has_children: true,
-                          children: [
-                            {
-                              name: "Bill Nye",
-                              age: 013,
-                              has_parent: true,
-                              is_leaf: true,
-                              has_children: false,
-                              children: []
-                            },
-                            {
-                              name: "Ann Druyan",
-                              age: 914,
-                              has_parent: true,
-                              is_leaf: true,
-                              has_children: false,
-                              children: []
-                            },
-                            {
-                              name: "Jon Stewart",
-                              age: 841,
-                              has_parent: true,
-                              is_leaf: true,
-                              has_children: false,
-                              children: []
-                            }
-                          ]
-                        },
-                        {
-                          name: "Aaron Turner",
-                          age: 33,
-                          has_parent: true,
-                          is_leaf: true,
-                          has_children: false,
-                          children: []
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              name: "Mary",
-              age: 910,
-              has_parent: true,
-              is_leaf: false,
-              has_children: true,
-              children: [
-                {
-                  name: "Stevie",
-                  age: 66,
-                  has_parent: true,
-                  is_leaf: false,
-                  has_children: true,
-                  children: [
-                    {
-                      name: "Michelle",
-                      age: 22,
-                      has_parent: true,
-                      is_leaf: false,
-                      has_children: true,
-                      children: [
-                        {
-                          name: "Douglas Crockford",
-                          age: 88,
-                          has_parent: true,
-                          is_leaf: true,
-                          has_children: false,
-                          children: []
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
-          },
-        ]
-      },
-      {
-        name: "Victor Von Doom",
-        age: 500,
-        has_parent:true,
+    children: [{
+      name: "Manny",
+      age: 22,
+      has_parent: true,
+      is_leaf: false,
+      has_children: true,
+      children: [{
+        name: "Spencer Toyama",
+        age: 28,
+        has_parent: false,
         is_leaf: true,
         has_children: false,
         children: []
-      }
-    ]
+      }, {
+        name: "Shannon",
+        age: 65,
+        has_parent: true,
+        is_leaf: false,
+        has_children: true,
+        children: [{
+          name: "Kingtak",
+          age: 302,
+          has_parent: true,
+          is_leaf: false,
+          has_children: true,
+          children: [{
+            name: "Wray",
+            age: 1337,
+            has_parent: true,
+            is_leaf: false,
+            has_children: true,
+            children: [{
+              name: "Edgar Allan Poe",
+              age: 560,
+              has_parent: true,
+              is_leaf: true,
+              has_children: false,
+              children: []
+            }, {
+              name: "Albert Einstein",
+              age: 91,
+              has_parent: true,
+              is_leaf: true,
+              has_children: false,
+              children: []
+            }, ]
+          }, {
+            name: "Nikola Tesla",
+            age: 16,
+            has_parent: true,
+            is_leaf: true,
+            has_children: false,
+            children: []
+          }]
+        }, {
+          name: "Peter",
+          age: 36,
+          has_parent: true,
+          is_leaf: false,
+          has_children: true,
+          children: [{
+            name: "Garfield The Cat",
+            age: 783,
+            has_parent: true,
+            is_leaf: true,
+            has_children: false,
+            children: []
+          }, {
+            name: "Cortney",
+            age: 243,
+            has_parent: true,
+            is_leaf: false,
+            has_children: true,
+            children: [{
+              name: "Brian",
+              age: 32,
+              has_parent: true,
+              is_leaf: false,
+              has_children: true,
+              children: [{
+                name: "Eylem",
+                age: 24,
+                has_parent: true,
+                is_leaf: false,
+                has_children: true,
+                children: [{
+                  name: "Milton S. Hershey",
+                  age: 8008,
+                  has_parent: true,
+                  is_leaf: true,
+                  has_children: false,
+                  children: []
+                }]
+              }, {
+                name: "Albert",
+                age: 87,
+                has_parent: true,
+                is_leaf: false,
+                has_children: true,
+                children: [{
+                  name: "Charles",
+                  age: 0,
+                  has_parent: true,
+                  is_leaf: false,
+                  has_children: true,
+                  children: [{
+                    name: "Gene Shoemaker",
+                    age: 62,
+                    has_parent: true,
+                    is_leaf: true,
+                    has_children: false,
+                    children: []
+                  }]
+                }, {
+                  name: "Neil deGrasse Tyson",
+                  age: 65,
+                  has_parent: true,
+                  is_leaf: true,
+                  has_children: false,
+                  children: []
+                }]
+              }]
+            }, {
+              name: "Carl Sagan",
+              age: 23,
+              has_parent: true,
+              is_leaf: true,
+              has_children: false,
+              children: []
+            }]
+          }, {
+            name: "Mark",
+            age: 100,
+            has_parent: true,
+            is_leaf: false,
+            has_children: true,
+            children: [{
+              name: "Marty McFly",
+              age: 43,
+              has_parent: true,
+              is_leaf: true,
+              has_children: false,
+              children: []
+            }, {
+              name: "Jayson",
+              age: 817,
+              has_parent: true,
+              is_leaf: false,
+              has_children: true,
+              children: [{
+                name: "Bill Nye",
+                age: 13,
+                has_parent: true,
+                is_leaf: true,
+                has_children: false,
+                children: []
+              }, {
+                name: "Ann Druyan",
+                age: 914,
+                has_parent: true,
+                is_leaf: true,
+                has_children: false,
+                children: []
+              }, {
+                name: "Jon Stewart",
+                age: 841,
+                has_parent: true,
+                is_leaf: true,
+                has_children: false,
+                children: []
+              }]
+            }, {
+              name: "Aaron Turner",
+              age: 33,
+              has_parent: true,
+              is_leaf: true,
+              has_children: false,
+              children: []
+            }]
+          }]
+        }]
+      }, {
+        name: "Mary",
+        age: 910,
+        has_parent: true,
+        is_leaf: false,
+        has_children: true,
+        children: [{
+          name: "Stevie",
+          age: 66,
+          has_parent: true,
+          is_leaf: false,
+          has_children: true,
+          children: [{
+            name: "Michelle",
+            age: 22,
+            has_parent: true,
+            is_leaf: false,
+            has_children: true,
+            children: [{
+              name: "Douglas Crockford",
+              age: 88,
+              has_parent: true,
+              is_leaf: true,
+              has_children: false,
+              children: []
+            }]
+          }]
+        }]
+      }]
+    }, {
+      name: "Victor Von Doom",
+      age: 500,
+      has_parent: true,
+      is_leaf: true,
+      has_children: false,
+      children: []
+    }]
   }
 };
 
@@ -298,4 +247,3 @@ module.exports = {
 float downward toward the earth
       rest in my bonsai
 */
-
