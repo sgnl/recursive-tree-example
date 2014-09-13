5248bbd493bfbc7bf9e88b8227b276b88abd96f4
Ray Farias <ray@rayfarias.com>
2014-09-13 10:30:57 -1000

    add test file

---
+++ test/recursive.spec.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+var elevator = require('./../tree.js');
+
+describe('Traversing a Tree', function () {
+  it('should exist', function () {
+
+
+  });
+}); // ends Traversing... desc
