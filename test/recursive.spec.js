9540ce6a6c48c845bfe2a2281be352601e338277
Ray Farias <ray@rayfarias.com>
2014-09-13 13:55:20 -1000

    add base specs; set up second spec

--- test/recursive.spec.js
+++ test/recursive.spec.js
@@ -1,11 +1,44 @@
 var chai = require('chai');
 var expect = chai.expect;
-var tree = require('./../tree.js');
-var traverse = require(/* require your work here */);
+var tree = require('../tree');
+var traverse = require('../traverse');
 
-describe('Traversing a Tree', function () {
-  it('should exist', function () {
+describe('Traversing a Tree', function(){
+  describe('Methods should exist', function(){
+    var methodList = [
+      'getAllNames',
+      'getAllAges',
+      'getLeafNames',
+      'getLeafAges',
+      'findAllParentsName',
+      'findAllParentsAge',
+      'findName',
+      'findAge'
+    ];
 
+    for (var i = 0; i < methodList.length; i++){
+      checkMethodExistance(methodList[i]);
+    }
+  });// ends Methods exists... desc
 
+  describe('Method getAllNames', function(){
+    beforeEach(function(){
+      var 
+    });
+
+      it('should return an array', function(){
+        
+      });
   });
 }); // ends Traversing... desc
+
+
+/* **************** *
+ * helper functions *
+ * **************** */
+function checkMethodExistance(methodName){
+  it(methodName + ' should be defined and be a Function', function(){
+    expect(traverse[methodName]).to.not.be.undefined;
+    expect(traverse[methodName]).to.a.instanceOf(Function);
+  });
+}
---
+++ traverse.js
@@ -0,0 +1,16 @@
+
+'use strict';
+var traverse = {};
+
+traverse.getAllNames = function(){};
+traverse.getAllAges = function(){};
+traverse.getLeafNames = function(){};
+traverse.getLeafAges = function(){};
+traverse.findAllParentsName = function(){};
+traverse.findAllParentsAge = function(){};
+traverse.findName = function(){};
+traverse.findAge = function(){};
+
+
+
+module.exports = traverse;
