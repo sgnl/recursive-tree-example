d89f4d520199fed89d2d43bd20a527b14de5ed50
Ray Farias <ray@rayfarias.com>
2014-09-13 10:30:28 -1000

    add tree data

---
+++ tree.js
@@ -0,0 +1,218 @@
+
+
+'use strict';
+// a tree to be climbed.
+module.export = {
+  root: {
+    has_parent: null,
+    has_children: true,
+    children: [
+      {
+        has_parent: true,
+        is_leaf: false,
+        has_children: true,
+        children: [
+          {
+            {
+              has_parent: false,
+              is_leaf: true,
+              has_children: false,
+              children: []
+            },
+            {
+              has_parent: true,
+              is_leaf: false,
+              has_children: true,
+              children: [
+                {
+                  has_parent: true,
+                  is_leaf: false,
+                  has_children: true,
+                  children: [
+                    {
+                      has_parent: true,
+                      is_leaf: false,
+                      has_children: true,
+                      children: [
+                        {
+                          has_parent: true,
+                          is_leaf: true,
+                          has_children: false,
+                          children: []
+                        },
+                        {
+                          has_parent: true,
+                          is_leaf: true,
+                          has_children: false,
+                          children: []
+                        },
+                      ]
+                    },
+                    {
+                      has_parent: true,
+                      is_leaf: true,
+                      has_children: false,
+                      children: []
+                    }
+                  ]
+                },
+                {
+                  has_parent: true,
+                  is_leaf: false,
+                  has_children: true,
+                  children: [
+                    {
+                      has_parent: true,
+                      is_leaf: true,
+                      has_children: false,
+                      children: []
+                    },
+                    {
+                      has_parent: true,
+                      is_leaf: false,
+                      has_children: true,
+                      children: [
+                        {
+                          has_parent: true,
+                          is_leaf: false,
+                          has_children: true,
+                          children: [
+                            {
+                              has_parent: true,
+                              is_leaf: false,
+                              has_children: true,
+                              children: [
+                                {
+                                  has_parent: true,
+                                  is_leaf: true,
+                                  has_children: false,
+                                  children: []
+                                }
+                              ]
+                            },
+                            {
+                              has_parent: true,
+                              is_leaf: false,
+                              has_children: true,
+                              children: [
+                                {
+                                  has_parent: true,
+                                  is_leaf: false,
+                                  has_children: true,
+                                  children: [
+                                    {
+                                      has_parent: true,
+                                      is_leaf: true,
+                                      has_children: false,
+                                      children: []
+                                    }
+                                  ]
+                                },
+                                {
+                                  has_parent: true,
+                                  is_leaf: true,
+                                  has_children: false,
+                                  children: []
+                                }
+                              ]
+                            }
+                          ]
+                        },
+                        {
+                          has_parent: true,
+                          is_leaf: true,
+                          has_children: false,
+                          children: []
+                        }
+                      ]
+                    },
+                    {
+                      has_parent: true,
+                      is_leaf: false,
+                      has_children: true,
+                      children: [
+                        {
+                          has_parent: true,
+                          is_leaf: true,
+                          has_children: false,
+                          children: []
+                        },
+                        {
+                          has_parent: true,
+                          is_leaf: false,
+                          has_children: true,
+                          children: [
+                            {
+                              has_parent: true,
+                              is_leaf: true,
+                              has_children: false,
+                              children: []
+                            },
+                            {
+                              has_parent: true,
+                              is_leaf: true,
+                              has_children: false,
+                              children: []
+                            },
+                            {
+                              has_parent: true,
+                              is_leaf: true,
+                              has_children: false,
+                              children: []
+                            }
+                          ]
+                        },
+                        {
+                          has_parent: true,
+                          is_leaf: true,
+                          has_children: false,
+                          children: []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              has_parent: true,
+              is_leaf: false,
+              has_children: true,
+              children: [
+                {
+                  has_parent: true,
+                  is_leaf: false,
+                  has_children: true,
+                  children: [
+                    {
+                      has_parent: true,
+                      is_leaf: false,
+                      has_children: true,
+                      children: [
+                        {
+                          has_parent: true,
+                          is_leaf: true,
+                          has_children: false,
+                          children: []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          },
+        ]
+      },
+      {
+        has_parent:true,
+        is_leaf: true,
+        has_children: false,
+        children: []
+      },
+    ]
+  }
+}
+// Still beneath the snow,
+// The bonsai'd elm makes spring plans:
+// "Leaves, I think, this year."
