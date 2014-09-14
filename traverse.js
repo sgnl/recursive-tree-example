6d5a528e81897f86452c8a7bc092e7102f832810
Ray Farias <ray@rayfarias.com>
2014-09-13 20:49:54 -1000

    implement first method functionality

--- traverse.js
+++ traverse.js
@@ -11,7 +11,45 @@ function Traverse(data){
  * uncomment the code below, then remove this comment
  */
 
-// Traverse.prototype.getAllNames = function(){};
+Traverse.prototype.getAllNames = function(currentNode, resultsArray){
+  this.results = resultsArray || [];
+  var _tree = this.tree;
+  var node = (function(){
+    if (!currentNode) {
+      return _tree.root;
+    } else {
+      return currentNode;
+    }
+  })(_tree);
+
+  // console.log(node.name);
+  // this.results.push(node.name);
+  this.pushValue(node, 'name');
+
+  if (node.has_children) {
+    for (var i = 0; i < node.children.length; i++) {
+      this.getAllNames(node.children[i], this.results);
+    }
+  }
+
+  return this.results;
+};
+
+Traverse.prototype.getAllAges = function(){};
+Traverse.prototype.getLeafNames = function(){};
+Traverse.prototype.getLeafAges = function(){};
+Traverse.prototype.findAllParentsNames = function(){};
+Traverse.prototype.findAllParentsAge = function(){};
+Traverse.prototype.findName = function(){};
+Traverse.prototype.findAge = function(){};
+
+/******************
+ * Helper Methods *
+ ******************/
+
+Traverse.prototype.pushValue = function(obj, val){
+  this.results.push(obj[val]);
+};
 
 /* exports the Class Traverse */
 module.exports = Traverse;
